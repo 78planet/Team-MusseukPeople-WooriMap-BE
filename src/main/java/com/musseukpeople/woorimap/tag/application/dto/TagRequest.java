154d4531601dd2861c2babd5cd9d214ea3643fed
will kim <78planet@gmail.com>
2022-08-06 17:01:12 +0900

    [WOOR-38] feat: 게시글 수정 API - TagRequest 제약 사항 추가

--- src/main/java/com/musseukpeople/woorimap/tag/application/dto/TagRequest.java
+++ src/main/java/com/musseukpeople/woorimap/tag/application/dto/TagRequest.java
@@ -1,6 +1,7 @@
 package com.musseukpeople.woorimap.tag.application.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 public class TagRequest {
 
     @NotBlank
+    @Size(max = 10, message = "태그 이름은 최대 10자입니다.")
     private String name;
 
+    @Size(max = 7, message = "태그 컬러 길이는 최대 7자입니다.")
     private String color;
 
     public TagRequest(String name, String color) {
